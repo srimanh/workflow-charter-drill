3189b72ecb009a3d2f65c36fe15027af195e72e7
sriman <srimandgl2004@gmail.com>
2026-05-14 23:10:42 +0530

    urgent fix

--- src/payments.js
+++ src/payments.js
@@ -1,7 +1,7 @@
 function calculateTotal(items) {
     let total = 0;
     for (let i = 0; i < items.length; i++) {
-        total += items[i].price;
+        total += (items[i].price * 1);
     }
     return total;
 }
