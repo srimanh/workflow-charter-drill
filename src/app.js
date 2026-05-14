864113a7e73961cca74eae866d29c13d78fc5db1
sriman <srimandgl2004@gmail.com>
2026-05-14 23:10:43 +0530

    final final

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('API is running');
+  res.send('API server is active');
 });
 
 app.get('/status', function(req, res) {
